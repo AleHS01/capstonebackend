e72704daa7108d43a2eada637d23ad78d25d28ec
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-11 21:24:24 -0400

    add appropriate middleware in index.js

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,37 @@
-const express = require('express')
 const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
 const User = require('../database/Models/user')
 
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-      User.findOne({ where: username }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (!user.verifyPassword(password)) { return done(null, false); }
-        return done(null, user);
-      });
+passport.use(new LocalStrategy (
+    async(username, password, done) => {
+        try {
+            const user = User.findOne({where:{username}})
+            if(!user){
+                return done(null, false, {message: "User Not found!"})
+            }
+            const validUser = await bcrypt.compare(password, user.password)
+            if(!validUser){
+                return done(null, false, {message: "Invalid password"})
+            }
+            return done(null,user)
+        } catch (error) {
+            done(error)
+        }
     }
-  ));
+))
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findByPk(id).then((user)=> {
+        done(null,user)
+    }).catch((error)=> {
+        done(error)
+    })
+})
+
+module.exports = passport
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const passport = require('./config/passport')
 const cors = require("cors");
 const app = express();
 const PORT = 8080;
 const db = require("./database/db.js");
 app.use(cors());
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// express-session middleware 
+app.use(require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+
+// Initialize passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get("/", (req, res) => {
   res.send("server up and running :)");
