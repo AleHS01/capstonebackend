f2eeeee706973be0a42a8cf726cf1619e433da0a
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-11 21:29:28 -0400

    add seed file

---
+++ seed.js
@@ -0,0 +1,12 @@
+const User = require('./database/Models/user')
+
+const seedUser = [
+    {username: "hamza", email: "hamzakhaliq@gmail.com", password: "hikhal56"},
+    {username: "Ale", email: "Ale@gmail.com", password: "ale56"}
+]
+
+const seed = async () => {
+    await User.bulkCreate(seedUser);
+  };
+
+  seed().then(()=>process.exit);
