1b0f49cc6c333d1c6741551e17c8506c83fe90fa
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-07-11 20:20:21 -0400

    setting up the db connection

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const PORT = 8080
-app.use(cors())
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 8080;
+const db = require("./database/db.js");
+app.use(cors());
 
-app.get('/', (req, res)=>{
-    res.send("server up and running :)")
-})
+app.get("/", (req, res) => {
+  res.send("server up and running :)");
+});
 
-const runServer = () => {
-    app.listen(PORT, ()=>{
-        console.log("Live on port 8080.")
-    })
-}
+// app.use("/api", require("./api"));
 
-runServer()
-module.exports = app
+const runServer = async () => {
+  await db.sync();
+  app.listen(PORT, () => {
+    console.log("Live on port 8080.");
+  });
+};
+
+runServer();
+module.exports = app;
