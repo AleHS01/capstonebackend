8bf42cab3de6b453217fd6d1fb35fb187bd76707
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-11 20:58:02 -0400

    add beforeCreate and beforeUpdate

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const passport = require('passport')
-const User = require('../database/db')
+const User = require('../database/Models/user')
 
 passport.use(new LocalStrategy(
     function(username, password, done) {
-      User.findOne({ username: username }, function (err, user) {
+      User.findOne({ where: username }, function (err, user) {
         if (err) { return done(err); }
         if (!user) { return done(null, false); }
         if (!user.verifyPassword(password)) { return done(null, false); }
--- database/Models/user.js
+++ database/Models/user.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require('sequelize/types')
 const db = require('../db')
 const {DataTypes} = require('sequelize')
+const bcrypt = require('bcrypt')
 
 const User = db.define("Users", {
     username:{
@@ -18,6 +19,21 @@ const User = db.define("Users", {
         allowNull: false,
         unique: true
     }
-})
+},{
+    hooks: {
+        // trigger beforeCreate hook --> when an entry is saved for the first time
+        beforeCreate: async (user) => {
+            if(user.password){
+                const salt = await bcrypt.genSalt(10) // generating salt using bcrypt
+                user.password = await bcrypt.hash(user.password, salt)
+            }
+        },
+        beforeUpdate: async (user) => {
+            const salt = await bcrypt.genSalt(10) // generating salt using bcrypt
+            user.password = await bcrypt.hash(user.password, salt)
+        }
+    }
+}
+)
 
 module.exports = User
