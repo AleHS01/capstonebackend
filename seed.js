2e69df6188f0bf225c9b75ee6a035e30d8da04d7
Ghulam Nabi <ghualmnabia75@gmail.com>
2023-07-16 18:21:22 -0400

    Ghulam DB setup

--- seed.js
+++ seed.js
@@ -3,6 +3,7 @@ const User = require("./database/Models/user");
 const seedUser = [
   { username: "hamza", email: "hamzakhaliq@gmail.com", password: "hikhal56" },
   { username: "Ale", email: "Ale@gmail.com", password: "ale56" },
+  { username: "Ghulam", email: "ghulmanabia75@gmail.com", password: "Home6924" },
 ];
 
 const seed = async () => {
