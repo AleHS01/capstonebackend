1100707e302c3caa6c40e86cfd92388e4cedf91b
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-16 17:43:45 -0400

    Create Budget Model

--- database/Models/index.js
+++ database/Models/index.js
@@ -1,15 +1,19 @@
 const User = require("./user");
 const Expense = require("./expense");
 const Income = require("./income")
+const Budget = require("./budget")
 
 //Associations
 User.hasMany(Expense);
 User.hasMany(Income)
+User.hasMany(Budget)
+Budget.hasOne(User)
 Expense.belongsTo(User);
 Income.belongsTo(User)
 
 module.exports = {
   User,
   Expense,
-  Income
+  Income,
+  Budget
 };
