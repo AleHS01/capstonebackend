dfa6b1acd9a0e3e224472effb55dc01d0fe159e2
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2023-07-22 08:13:11 -0400

    Adding a create group route

--- api/group.js
+++ api/group.js
@@ -1,14 +1,22 @@
 const router = require("express").Router();
-const {Group} = require("../database/Models");
+const {Group,User} = require("../database/Models");
 const authenticateUser = require("../middleware/authenticateUser")
 
-router.post("/add",authenticateUser,async(req,res,next)=>{
+router.post("/create",authenticateUser,async(req,res,next)=>{
     try {
         const userID=req.user.id;
         const {name}=req.body;
+
+        const user= await User.findByPk(userID)
+        
         const new_group=await Group.create({
-            name
+            group_name:name
         });
+
+        await user.update({
+            GroupId: new_group.id,
+          });
+      
 
         res.status(200).json(new_group)
         
--- database/Models/group.js
+++ database/Models/group.js
@@ -2,10 +2,6 @@ const { DataTypes } =require("sequelize")
 const db = require("../db")
 
 const Group = db.define("Group",{
-    group_id:{
-        type:DataTypes.INTEGER,
-        primaryKey:true
-    },
     group_name:{
         type:DataTypes.STRING,
         allowNull: true,
