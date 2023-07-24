f92a409efa0ef3c619d61ae99cadd841d3763e40
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2023-07-24 07:33:40 -0400

    Adding an /all_members route to retreive all the users of a specific user

--- api/group.js
+++ api/group.js
@@ -74,4 +74,17 @@ router.get("/user_hasGroup",authenticateUser,async(req,res,next)=>{
     }
 })
 
+router.get("/all_members",authenticateUser,async (req,res,next)=>{
+    try {
+        const GroupId= req.user.GroupId
+        const users =await User.findAll({where:{
+            GroupId
+        }})
+        res.status(200).json(users)
+
+    } catch (error) {
+        next(error)
+    }
+})
+
 module.exports= router;
