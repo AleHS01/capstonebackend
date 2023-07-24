e14409c23c40181caf96bef444596aff896c33c7
Ghulam Nabi <ghualmnabia75@gmail.com>
2023-07-24 00:21:20 -0400

    Ghulam's DB setup

--- database/db.js
+++ database/db.js
@@ -2,10 +2,10 @@ const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv").config();
 const pg = require("pg");
 
-const db = new Sequelize("postgres://postgres@localhost:5432/financemate_db", {
-  logging: false,
-  dialectModule: pg,
-});
+// const db = new Sequelize("postgres://postgres@localhost:5432/financemate_db", {
+//   logging: false,
+//   dialectModule: pg,
+// });
 //const db = new Sequelize('postgres://localhost:5432/capstone_database')
 
 // const db = new Sequelize("postgres://postgres:Home6924@localhost:5432/financemate_db", {
@@ -14,10 +14,10 @@ const db = new Sequelize("postgres://postgres@localhost:5432/financemate_db", {
 // });
 // const db = new Sequelize('postgres://localhost:5432/capstonedatabase')
 
-// const db = new Sequelize("postgres://postgres:Home6924@localhost:5432/financemate_db", {
-//   logging: false,
-//   dialectModule: pg,
-// });
+const db = new Sequelize("postgres://postgres:Home6924@localhost:5432/financemate_db", {
+  logging: false,
+  dialectModule: pg,
+});
 
 //Test Conection
 db.authenticate()
