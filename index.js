a76afa02be402107a49bbe00d7caaec5cd7cb2d0
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-07-26 22:52:02 -0400

    deploy 20

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
-const passportLocalStrategy = require("passport-local").Strategy;
+// const passportLocalStrategy = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
-const cookieSession = require("cookie-session");
-const bycrypt = require("bcryptjs");
+// const cookieSession = require("cookie-session");
+// const bycrypt = require("bcryptjs");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const db = require("./database/db");
@@ -19,23 +19,25 @@ const sessionStore = new SequelizeStore({ db });
 // MiddleWare
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
+// const corsOptions = {
 
-    // Check if the origin is in the whitelist (replace 'http://localhost:3000' with your actual frontend URL)
-    const allowedOrigins = ["http://localhost:3000"];
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+
+//     // Check if the origin is in the whitelist (replace 'http://localhost:3000' with your actual frontend URL)
+//     const allowedOrigins = ["http://localhost:3000"];
+//     if (allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// };
 
 // app.use(cors(corsOptions)); // Use the custom CORS middleware
+
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -53,7 +55,7 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: {
-      maxAge: 8 * 60 * 60 * 1000,
+      maxAge: 1000 * 60 * 60 * 24,
       httpOnly: true,
       // sameSite: "none", ONLY USE WHEN DEPLOY
       // secure: true,
@@ -72,19 +74,19 @@ app.use(
 //     httpOnly: true,
 //   })
 // );
-app.use(function (request, response, next) {
-  if (request.session && !request.session.regenerate) {
-    request.session.regenerate = (cb) => {
-      cb();
-    };
-  }
-  if (request.session && !request.session.save) {
-    request.session.save = (cb) => {
-      cb();
-    };
-  }
-  next();
-});
+// app.use(function (request, response, next) {
+//   if (request.session && !request.session.regenerate) {
+//     request.session.regenerate = (cb) => {
+//       cb();
+//     };
+//   }
+//   if (request.session && !request.session.save) {
+//     request.session.save = (cb) => {
+//       cb();
+//     };
+//   }
+//   next();
+// });
 
 app.use(passport.initialize());
 app.use(passport.session());
