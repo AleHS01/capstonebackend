3ee52ebf5294d1eacf75cc222c74c284e8b91851
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-07-27 11:03:41 -0400

    deploy 22

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -1,7 +1,7 @@
-const authenticateUser = (req, res, next) => {
-  console.log("is auth?", req.isAuthenticated());
-  console.log("req.user ", req.user);
-  if (req.isAuthenticated()) {
+const authenticateUser = async (req, res, next) => {
+  console.log("is auth?", await req.isAuthenticated());
+  console.log("req.user ", await req.user);
+  if (await req.isAuthenticated()) {
     // If the user is authenticated, allow them to proceed
     return next();
   }
