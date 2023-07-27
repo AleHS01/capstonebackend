bea7d2ac8845f88ba4ed3ab89a3c9d90650630a7
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-07-27 14:15:28 -0400

    deploy 32

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const cookieParser = require("cookie-parser");
 // const cookieSession = require("cookie-session");
 // const bycrypt = require("bcryptjs");
 const bodyParser = require("body-parser");
+const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
@@ -55,7 +57,7 @@ app.use(
   session({
     secret: "secret",
     store: sessionStore,
-    resave: false,
+    resave: true,
     saveUninitialized: true,
     // cookie: {
     //   maxAge: 1000 * 60 * 60 * 24,
@@ -122,7 +124,94 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-require("./config/passportConfig")(passport); //to use same instance of passport in the entire server
+// require("./config/passportConfig")(passport); //to use same instance of passport in the entire server
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ where: { username } });
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect Username" });
+      }
+      if (!bcrypt.compare(password, user.password)) {
+        return done(null, false, { message: "Incorrect Password" });
+      }
+      // await bcrypt.compare(password, user.password, (error, result) => {
+      //   if (error) {
+      //     return done(error);
+      //   }
+
+      //   if (result === true) {
+      //     return done(null, user);
+      //   } else {
+      //     return done(null, false, { message: "Incorrect Password" });
+      //   }
+      // });
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+// -------------Google Auth--------------
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:8080/api/login/google_callback",
+      // passReqToCallback: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const defaultUser = {
+          username: `${profile.name.givenName} ${profile.name.familyName}`,
+          email: profile.emails[0].value,
+          googleId: profile.id,
+          first_name: profile.name.givenName,
+          last_name: profile.name.familyName,
+        };
+
+        // console.log("google Profile:", profile);
+        const [user] = await User.findOrCreate({
+          where: { googleId: defaultUser.googleId },
+          defaults: defaultUser,
+        });
+        done(null, user);
+      } catch (error) {
+        done(error);
+      }
+      // ).catch((err) => {
+      //   console.log(err);
+      //   done(err, null);
+      // });
+      // if (user && user[0]) {
+      //   return done(null, user && user[0]);
+      // }
+    }
+  )
+);
+
+// -------------End of Google Auth--------------
+
+passport.serializeUser((user, cb) => {
+  console.log("Serialize user id:", user.id);
+  cb(null, user.id);
+});
+
+passport.deserializeUser(async (id, cb) => {
+  console.log("User id in deserializeUser: ", id);
+  try {
+    const user = await User.findByPk(id);
+    cb(null, user);
+  } catch (error) {
+    cb(error, null);
+  }
+});
 
 // app.use(cookieParser("secret"));
 
